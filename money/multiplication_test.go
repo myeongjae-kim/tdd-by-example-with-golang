3fecb324ce1bc9a65c9aa6829dc0146ee50ceec0
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-01-27 00:05:00 +0900

    [Chapter 2] Succeeded to compile but a test is failed.

--- money/multiplication_test.go
+++ money/multiplication_test.go
@@ -5,8 +5,12 @@ import "testing"
 // TestMultiplication is a test to test multiplcation.
 func TestMultiplication(t *testing.T) {
 	five := Dollar{5}
-	five.times(2)
-	if five.amount != 10 {
-		t.Error("wrong result")
+	product := five.times(2)
+	if product.amount != 10 {
+		t.Errorf("wrong result, expected: %d, result: %d", 10, product.amount)
+	}
+	product = five.times(3)
+	if product.amount != 15 {
+		t.Errorf("wrong result, expected: %d, result: %d", 15, product.amount)
 	}
 }
