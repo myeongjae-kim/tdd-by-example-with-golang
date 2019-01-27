1af66e7ab87f1c08c16cd46b1a6a589df556c2b4
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-01-27 18:51:16 +0900

    [Chapter 9] Add Currency method

--- money/internal/pkg/money/dollar.go
+++ money/internal/pkg/money/dollar.go
@@ -5,11 +5,11 @@ type dollar struct {
 	Money
 }
 
-func (d *dollar) times(multiplier int) dollar {
+func (d dollar) times(multiplier int) dollar {
 	return NewDollar(d.GetAmount() * multiplier)
 }
 
 // NewDollar is a constructor of Dollar package.
 func NewDollar(amount int) dollar {
-	return dollar{NewMoney(amount)}
+	return dollar{NewMoney(amount, "USD")}
 }
